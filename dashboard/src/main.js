75f0bc32203b21ca9228d8686f406139e2993f24
GonzaloHirsch <hirschgonzalo@gmail.com>
2024-04-27 22:47:07 +0100

    fix: incorrect import path

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -7,7 +7,7 @@ let config = undefined;
 
 createApp(App, {
   port: async () => {
-    if (!config) config = await import('../../viz.config.json')
+    if (!config) config = await import('../viz.config.json')
     return config.http.port
   }
 })
